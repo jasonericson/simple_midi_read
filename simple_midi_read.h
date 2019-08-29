5e27e915f2e811f1eaabef890ab468c37d32d3ec
Jason Ericson <jason@jasonericson.net>
2019-08-28 21:59:51 -0700

    Getting MIDI file format.

--- simple_midi_read.h
+++ simple_midi_read.h
@@ -2,13 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* #define VERBOSE_DEBUGGING */
+
 /* TODO: Check for endian-ness */
-int get_next_int(char* buffer_read)
+int get_next_int(char** buffer_read)
 {
     int result;
 
-    result = buffer_read[3] | ((int)buffer_read[2] << 8) | ((int)buffer_read[1] << 16) | ((int)buffer_read[0] << 24);
-    buffer_read += 4;
+#ifdef VERBOSE_DEBUGGING
+    printf("Getting next int.\n");
+    printf("Byte 0: %.2x\n", (*buffer_read)[0] & 0xff);
+    printf("Byte 1: %.2x\n", (*buffer_read)[1] & 0xff);
+    printf("Byte 2: %.2x\n", (*buffer_read)[2] & 0xff);
+    printf("Byte 3: %.2x\n", (*buffer_read)[3] & 0xff);
+#endif
+
+    result = (*buffer_read)[3] | ((int)(*buffer_read)[2] << 8) | ((int)(*buffer_read)[1] << 16) | ((int)(*buffer_read)[0] << 24);
+    *buffer_read += 4;
+
+    return result;
+}
+
+/* TODO: Figure out fixed-width implementation if possible. */
+short get_next_short(char** buffer_read)
+{
+    short result;
+
+#ifdef VERBOSE_DEBUGGING
+    printf("Getting next short.\n");
+    printf("Byte 0: %.2x\n", (*buffer_read)[0] & 0xff);
+    printf("Byte 1: %.2x\n", (*buffer_read)[1] & 0xff);
+#endif
+
+    result = (*buffer_read)[1] | ((short)(*buffer_read)[0] << 8);
+    *buffer_read += 2;
 
     return result;
 }
@@ -21,6 +48,7 @@ int read_midi_file(char* filename)
     char* buffer_read;
     int i;
     int header_chunklen;
+    short format;
 
     file_ptr = fopen(filename, "rb");
     if (!file_ptr)
@@ -51,12 +79,31 @@ int read_midi_file(char* filename)
     /* Check for header chunklen */
     /* TODO: Standardize when buffer pointer advances. */
     buffer_read += 4;
-    header_chunklen = get_next_int(buffer_read);
+    header_chunklen = get_next_int(&buffer_read);
     if (header_chunklen != 6)
     {
         printf("This MIDI file (%s) is not compatible with this version of the parser.\n", filename);
         /* VERBOSE */
-        printf("Expected header chunk size of 6 bytes, got %d.", header_chunklen);
+        printf("Expected header chunk size of 6 bytes, got %d.\n", header_chunklen);
+        return 1;
+    }
+
+    format = get_next_short(&buffer_read);
+    if (format == 0)
+    {
+        printf("This is a single-track MIDI file.\n");
+    }
+    else if (format == 1)
+    {
+        printf("This file contains two or more tracks meant to be played in tandem.\n");
+    }
+    else if (format == 2)
+    {
+        printf("This file contains one or more tracks meant to be played independently.\n");
+    }
+    else
+    {
+        printf("Do not recognize MIDI format %d.\n", format);
         return 1;
     }
 
