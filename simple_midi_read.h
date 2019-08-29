d309fb514fc26ef3f64dede8cfdf02401e01c52d
Jason Ericson <jason@jasonericson.net>
2019-08-28 21:43:53 -0700

    Getting header identifier and length.

--- simple_midi_read.h
+++ simple_midi_read.h
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/* TODO: Check for endian-ness */
+int get_next_int(char* buffer_read)
+{
+    int result;
+
+    result = buffer_read[3] | ((int)buffer_read[2] << 8) | ((int)buffer_read[1] << 16) | ((int)buffer_read[0] << 24);
+    buffer_read += 4;
+
+    return result;
+}
 
 int read_midi_file(char* filename)
 {
     FILE* file_ptr;
     long int file_size;
-    char* in_buffer;
+    char* full_buffer;
+    char* buffer_read;
     int i;
+    int header_chunklen;
 
     file_ptr = fopen(filename, "rb");
     if (!file_ptr)
     {
-        printf("Unable to open file!");
+        printf("Unable to open file!\n");
         return 1;
     }
 
@@ -21,23 +35,31 @@ int read_midi_file(char* filename)
     fseek(file_ptr, 0L, SEEK_SET);
 
     /* Read entire file */
-    in_buffer = (char*) malloc(file_size + 1);
-    fread(in_buffer, sizeof(char), file_size, file_ptr);
+    full_buffer = (char*) malloc(file_size + 1);
+    fread(full_buffer, sizeof(char), file_size, file_ptr);
+    fclose(file_ptr);
+    buffer_read = full_buffer;
 
-    for (i = 0; i < file_size; ++i)
+    /* Check for header identifier */
+    if (strncmp(buffer_read, "MThd", 4) != 0)
     {
-        printf("%.2x", in_buffer[i] & 0xff);
-        if (i % 16 == 15)
-        {
-            printf("\n");
-        }
-        else if (i % 2 == 1)
-        {
-            printf(" ");
-        }
+        /* TODO: Separate out verbose logging? */
+        printf("MIDI file is missing header identifier 'MThd'.\n");
+        return 1;
     }
 
-    printf("\n");
+    /* Check for header chunklen */
+    /* TODO: Standardize when buffer pointer advances. */
+    buffer_read += 4;
+    header_chunklen = get_next_int(buffer_read);
+    if (header_chunklen != 6)
+    {
+        printf("This MIDI file (%s) is not compatible with this version of the parser.\n", filename);
+        /* VERBOSE */
+        printf("Expected header chunk size of 6 bytes, got %d.", header_chunklen);
+        return 1;
+    }
 
+    printf("All good so far!\n");
     return 0;
 }
